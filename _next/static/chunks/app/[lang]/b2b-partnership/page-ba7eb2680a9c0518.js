3e632a10f56b1879080befbbd3153c2dfb498010
arturs-a <arturs.abele@kalmars.lv>
2025-05-01 15:05:55 +0300

    Updates

--- _next/static/chunks/app/[lang]/b2b-partnership/page-ba7eb2680a9c0518.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[681],{},function(n){n.O(0,[922,689,446,47,948,544,614,971,472,744],function(){return n(n.s=3614)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/[lang]/contact-us/page-5ee3038b658915cf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3],{},function(n){n.O(0,[922,4705,446,6691,825,482,2534,220,5874,2971,2472,1744],function(){return n(n.s=6800)}),_N_E=n.O()}]);
